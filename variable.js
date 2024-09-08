f657f901aec3d6bb30125459e21143295eb5c8a1
unknown <ceedriclazaro010205@gmail.com>
2024-09-08 08:10:42 -0800

    Initial commit with variable.js

---
+++ variable.js
@@ -0,0 +1,9 @@
+let name = "Ceedric L. Gacoscos";
+let whatDoYouWannaBecomeinYourLife = "programmer";
+let gender = "male";
+let twitterHandle = "@LazaroCeedric";
+
+console.log('My name is ', name);
+console.log('I want to become a ', whatDoYouWannaBecomeinYourLife);
+console.log('My gender is ', gender);
+console.log('My twitter handle is ', twitterHandle);
